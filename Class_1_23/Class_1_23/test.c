74dd66e9e01d29de7e6afbca018823ee971fd372
ZI1XUN <lvyizhe459@gmail.com>
2024-01-26 10:40:01 +0800

    测试github代码

---
+++ Class_1_23/Class_1_23/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+//c语言中一点要有main函数——主函数
+//主函数标准写法
+//int main()
+//{
+//	printf("%zu\n", sizeof(char));
+//	printf("%zu\n", sizeof(int));
+//	printf("%zu\n", sizeof(long));
+//	printf("%zu\n", sizeof(long long));
+//	printf("%zu\n", sizeof(short));
+//	printf("%zu\n", sizeof(float));
+//	printf("%zu\n", sizeof(double));
+//	return 0;
+//}
+//调用（包含）库函数
+#include <stdio.h>
+//stdio---/s-standard;/i-input;/o-output
+//运行程序快捷键--ctrl+F5
+
+//int main()
+//{
+//	int num1=0;
+//	int num2=0;
+//	scanf("%d %d",&num1,&num2);
+//	int sum = num1 + num2;
+//	printf("%d\n",sum);
+//	return 0;
+//}
+int main()
+{
+	printf(" % d", strlen("\r"));
+	return 0;
+}
